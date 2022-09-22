912b4be9d5ce01c05321fd54d08097a6c79f04cf
jongsungbae <jongsung.jaybae@gmail.com>
2022-09-21 22:17:46 -0400

    first suite

--- CypressAutomation/cypress/integration/examples/Test1.js
+++ CypressAutomation/cypress/integration/examples/Test1.js
@@ -5,7 +5,22 @@ describe('My First Suite', () => {
         cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
         cy.get('.search-keyword').type('ca')
         cy.wait(2000)
+
+        // check the number of the item
         cy.get('.product:visible').should('have.length', 4)
+        // parent child
+        cy.get('.products').find('.product').should('have.length', 4)
+
+        // click 'ADD TO CART' button
+        cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+
+        cy.get('.products').find('.product').each(($el, index, $list) => {
+            const textVeg = $el.find('h4.product-name').text()
+            if(textVeg.includes('Cashews')){
+                cy.wrap($el).find('button').click()
+            }
+        })
+       
     })
     
   })
