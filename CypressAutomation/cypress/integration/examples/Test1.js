4050470a5b99af5073ff7aca063e4a551f837f2f
jongsungbae <jongsung.jaybae@gmail.com>
2022-09-22 17:09:23 -0400

    check text

--- CypressAutomation/cypress/integration/examples/Test1.js
+++ CypressAutomation/cypress/integration/examples/Test1.js
@@ -9,18 +9,26 @@ describe('My First Suite', () => {
         // check the number of the item
         cy.get('.product:visible').should('have.length', 4)
         // parent child
-        cy.get('.products').find('.product').should('have.length', 4)
+        cy.get('.products').as('productLocator')
+        cy.get('@productLocator').find('.product').should('have.length', 4)
 
         // click 'ADD TO CART' button
-        cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+        cy.get('@productLocator').find('.product').eq(2).contains('ADD TO CART').click()
 
-        cy.get('.products').find('.product').each(($el, index, $list) => {
+        cy.get('@productLocator').find('.product').each(($el, index, $list) => {
             const textVeg = $el.find('h4.product-name').text()
             if(textVeg.includes('Cashews')){
                 cy.wrap($el).find('button').click()
             }
+        }) 
+
+        // assert if logo text is correctly displayed
+        cy.get('.brand').should('have.text', 'GREENKART')
+
+        // this is to print in logs
+        cy.get('.brand').then((logoelement) => {
+            cy.log(logoelement.text())
         })
-       
     })
     
   })
